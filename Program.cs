ba34dd548f52a88ccf0422d99a2c434774f31128
Alexandr <jwchaganav@gmail.com>
2022-10-22 15:02:13 +0400

    begin cube

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите координаты двух точек, сначала А: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-int y1 = Convert.ToInt32(Console.ReadLine());
+int[] A = GetArray("Введите три координаты точки А: ");
+int[] B = GetArray("Введите три координаты точки B: ");
 
-Console.WriteLine("Тепер координаты точки В: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-int y2 = Convert.ToInt32(Console.ReadLine());
-
-int sumTotal = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+int sumTotal = (B[0]-A[0])*(B[0]-A[0])+(B[1]-A[1])*(B[1]-A[1])+(B[2]-A[2])*(B[2]-A[2]);
 Console.WriteLine($"sumTotal: {sumTotal}");
 
 double result = Math.Sqrt(sumTotal);
-Console.WriteLine($"Растояние между точнами А({x1},{y1}) и B({x2},{y2}): {result}");
+Console.WriteLine($"Растояние между точнами А({A[0]},{A[1]},{A[2]}) и B({B[0]},{B[1]},{B[2]}): {result}");
+
+int[] GetArray (string message)
+{
+    int[] intArray = new int[3];
+    Console.WriteLine(message);
+    for (int i = 0; i < 3; i++)
+        intArray[i] = Convert.ToInt32(Console.ReadLine());
+    return intArray;
+}
